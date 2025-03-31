2c55c02528f4bed36fd69334b0244d567e25448b
David Naneishvili <davnub@yandex.ru>
2025-03-31 23:25:39 +0400

    implemented

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
-
 
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Errorf("failed to load file into memory: %s", err)
+		log.Fatalf("failed to load file into memory: %s\n", err)
 		return
 	}
-	data = make([]byte, 8)
 
-	for file.Read()
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buffer)
+		if err == io.EOF || n == 0 {
+			break
+		} else if err != nil {
+			log.Fatalf("Failed to read 8 bytes: %v\n", err)
+		}
+
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
 
 }
