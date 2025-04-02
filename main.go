caa8737eefc2d9bd546bd31f4ac4ffd87e11c2aa
David Naneishvili <davnub@yandex.ru>
2025-04-02 23:22:45 +0400

    did one

--- main.go
+++ main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
+	"strings"
 )
 
+const port string = ":42069"
+
 func main() {
-	file, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to load file into memory: %s\n", err)
-		return
 	}
 
-	defer file.Close()
-
-	buffer := make([]byte, 8)
+	defer listener.Close()
 
 	for {
-		n, err := file.Read(buffer)
-		if err == io.EOF || n == 0 {
-			break
-		} else if err != nil {
-			log.Fatalf("Failed to read 8 bytes: %v\n", err)
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Could not establish connection:", err)
 		}
+		fmt.Println("A new connection has been accepted")
+		c := getLinesChannel(conn)
+		for line := range c {
+			fmt.Println(line)
+		}
+		fmt.Println("Connection has been closed")
 
-		fmt.Printf("read: %s\n", string(buffer[:n]))
 	}
+
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		var curr string
+		defer file.Close()
+		defer close(c)
+		for {
+			buffer := make([]byte, 8)
+			n, err := file.Read(buffer)
+			if errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				log.Fatalf("Failed to read 8 bytes: %v\n", err)
+			}
+			parts := strings.Split(string(buffer[:n]), "\n")
+			for i, part := range parts {
+				curr += part
+				if i != len(parts)-1 {
+					c <- curr
+					curr = ""
+				}
+			}
+		}
+	}()
+	return c
 
 }
